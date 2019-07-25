18379eaf5721b70013f7d6e90a083c4206895e4e
choi-seung-min <3235tmdals@gmail.com>
2019-07-25 11:30:25 +0900

    Duh

--- app/src/main/java/com/example/note/ui/activity/EditActivity.kt
+++ app/src/main/java/com/example/note/ui/activity/EditActivity.kt
@@ -8,6 +8,5 @@ class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
-        //TODO: fix activity_edit like real note
     }
 }
